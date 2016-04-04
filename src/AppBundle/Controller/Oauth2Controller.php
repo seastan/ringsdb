9f8e97e06cbb9d210842c8e76e4b42bb4d3db778
Alberto Wagner <alberto.wagner@gmai.com>
2016-04-04 16:17:00 -0300

    Fixed API Docs

--- src/AppBundle/Controller/Oauth2Controller.php
+++ src/AppBundle/Controller/Oauth2Controller.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
 class Oauth2Controller extends Controller {
 	/**
