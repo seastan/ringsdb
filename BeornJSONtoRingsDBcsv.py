ba0b90eb8d04c6eadfc3911ae89b2b0268c681cb
chrisstanford <chris.j.stanford@gmail.com>
2018-12-07 10:28:32 -0800

    BeornParse update

--- BeornJSONtoRingsDBcsv.py
+++ BeornJSONtoRingsDBcsv.py
@@ -19,7 +19,7 @@ traitlist = ['Ally','Angmar','Archer','Armor','Armory','Armour','Arnor','Artifac
 nounlist = ['hero','character','ally','attachment','card','enemy','location']
 
 # RingsDB card types
-playercardtypes = ['Hero','Ally','Attachment','Event','Player Side Quest','Treasure','Boon'] 
+playercardtypes = ['Hero','Ally','Attachment','Event','Player Side Quest','Treasure','Boon']
 
 # Make "Action" and "Response" etc bold.
 def boldActions(text):
@@ -36,21 +36,57 @@ def boldTraits(text):
                         text = text.replace(trait+' '+noun,'<b><i>'+trait+'</i></b>'+' '+noun)
         return text
 
+# Format stats (attack/defense etc.) into the correct symbols
+def formatRingsdbStats(text):
+        text = text.replace("Attack","[attack]")
+        text = text.replace("Defense","[defense]")
+        text = text.replace("Willpower","[willpower]")
+        text = text.replace("Threat","[threat]")
+        text = text.replace("Leadership","[leadership]")
+        text = text.replace("Spirit","[spirit]")
+        text = text.replace("Tactics","[tactics]")
+        text = text.replace("Lore","[lore]")
+        text = text.replace("Baggins","[baggins]")
+        text = text.replace("Fellowship","[fellowship]")
+        return text
+
+def formatOctgnStats(text):
+        text = text.replace("Attack","Û")
+        text = text.replace("Defense","Ú")
+        text = text.replace("Willpower","Ò")
+        text = text.replace("Threat","$")
+        text = text.replace("Leadership","Ì")
+        text = text.replace("Spirit","Ê")
+        text = text.replace("Tactics","Ï")
+        text = text.replace("Lore","Î")
+        text = text.replace("Baggins","Í")
+        text = text.replace("Fellowship","☺")
+        return text
+
 # Format a card's text
-def formatText(text):
+def formatRingsdbText(text):
         text = re.sub(r"\.([A-z])",r". \1",text)
         text = text.replace("`","'")
         text = boldActions(text)
         text = boldTraits(text)
+        text = formatRingsdbStats(text)
         return text
-            
+
+def formatOctgnText(text):
+        text = formatOctgnStats(text)
+        return text
+
 # Open output csv file for writing
-beornCSV = open('BeornParsed.csv', 'w')
-csvwriter = csv.writer(beornCSV)
+ringsdbCSV = open('RingsDBset.csv', 'w')
+ringsdbwriter = csv.writer(ringsdbCSV)
+
+octgnCSV = open('Octgnset.csv', 'w')
+octgnwriter = csv.writer(octgnCSV)
 
 # Write the header line
 header = ['pack','type','sphere','position','code','name','traits','text','flavor','isUnique','cost','threat','willpower','attack','defense','health','victory','quest','quantity','deckLimit','illustrator','octgnid','hasErrata']       
-csvwriter.writerow(header)
+ringsdbwriter.writerow(header)
+
 
 # Loop over the cards in the JSON file
 for i,c in enumerate(beornJSON):
@@ -66,24 +102,27 @@ for i,c in enumerate(beornJSON):
         traits = traits.strip() # Remove leading/trailing whitespace
 
         # Text in ringsdb coposes both Keywords and regular card text
-        text = ''
+        plaintext = ''
         victory = ''
+        keywords = ''
         # Get keywords
         for keyword in c['Front']['Keywords']:
                 if 'Victory' in keyword:
                         victory = keyword.replace('Victory ','')
                         victory = victory.replace('.','')
                         continue
-                text = text+keyword+' '
-        text = text.strip() # Remove leading/trailing whitespace
-        # If there were keywords, add a newline
-        if len(text)>0:
-                text = text+'\n'
+                keywords = keywords+keyword+' '
+        keywords = keywords.strip() # Remove leading/trailing whitespace
         # Get card text
         for line in c['Front']['Text']:
-                text = text+line
+                plaintext = plaintext+line
         # Format text
-        text = formatText(text)
+        ringsdbtext = formatRingsdbText(plaintext)
+        octgntext = formatOctgnText(plaintext)
+
+        # For ringsdb we combine keywords with text
+        if len(keywords)>0:
+                ringsdbtext = keywords+'\n'+ringsdbtext
 
         # Flavor text
         flavor = c['Front']['FlavorText']
@@ -101,15 +140,21 @@ for i,c in enumerate(beornJSON):
 
         # Stats
         cost = ''
+        threatcost = ''
+        engcost = ''
         threat = ''
         willpower = ''
         attack = ''
         defense = ''
         health = ''
         quest = ''
+        shadow = ''
+        stagenum=''
+        stageletter=''
         if 'ResourceCost' in c['Front']['Stats'].keys(): cost = c['Front']['Stats']['ResourceCost']
-        if 'ThreatCost' in c['Front']['Stats'].keys(): threat = c['Front']['Stats']['ThreatCost']
-        elif 'Threat' in c['Front']['Stats'].keys(): threat = c['Front']['Stats']['Threat']
+        if 'EngagementCost' in c['Front']['Stats'].keys(): engcost = c['Front']['Stats']['EngagementCost']
+        if 'ThreatCost' in c['Front']['Stats'].keys(): threatcost = c['Front']['Stats']['ThreatCost']
+        if 'Threat' in c['Front']['Stats'].keys(): threat = c['Front']['Stats']['Threat']
         if 'Willpower' in c['Front']['Stats'].keys(): willpower = c['Front']['Stats']['Willpower']
         if 'Attack' in c['Front']['Stats'].keys(): attack = c['Front']['Stats']['Attack']
         if 'Defense' in c['Front']['Stats'].keys(): defense = c['Front']['Stats']['Defense']
@@ -119,18 +164,46 @@ for i,c in enumerate(beornJSON):
         # Decklimit
         limit = '3'
         if c['CardType'] == 'Hero': limit = '1'
-        elif '1 per deck' in text: limit = '1'
+        elif '1 per deck' in plaintext: limit = '1'
 
         # Errata
         haserrata = ''
         if c['HasErrata'] == 'true':
                 haserrata = '1'
                 
-        # Construct row to write to csv file
-        row = [c['CardSet'],c['CardType'],sphere,c['Number'],'',c['Title'],traits,text,flavor,isunique,cost,threat,willpower,attack,defense,health,victory,quest,c['Quantity'],limit,c['Artist'],'',haserrata]
+        # Construct row to write to ringsdb csv file
+        ringsdbrow = [c['CardSet'],c['CardType'],sphere,c['Number'],'',c['Title'],traits,ringsdbtext,flavor,isunique,cost,threatcost,willpower,attack,defense,health,victory,quest,c['Quantity'],limit,c['Artist'],'',haserrata]
+
+        # Additional considerations for Octgn
+        uniquesymbol = ''
+        if isunique:
+                uniquesymbol = '‰'
+
+        backoctgntext = ''
+        backquest = ''
+        backstagenum=''
+        backstageletter=''
+        backtype=''
+        backtitle=''
+        if c['CardType']=='Quest':
+                for line in c['Back']['Text']:
+                        plaintext = plaintext+line
+                backoctgntext = formatOctgnText(plaintext)
+                backtitle = c['Title']
+                backquest = quest
+                backtype = c['CardType']
+                quest = ''
+
+        encounterset = ''
+        if 'EncounterInfo' in c.keys():
+                if c['EncounterInfo'] and 'EncounterSet' in c['EncounterInfo'].keys():
+                        encounterset = c['EncounterInfo']['EncounterSet']
+        octgnrow = ['','','',c['Number'],c['Quantity'],encounterset,c['Title'],uniquesymbol,c['CardType'],sphere,traits,keywords,threatcost+cost,engcost,threat,willpower,attack,defense,health,quest,victory,octgntext,shadow,'',backtitle,'',backtype,'','','',backstagenum,backstageletter,'','','','','',backquest,'',backoctgntext]
 
         # Write row
-        csvwriter.writerow(row)
+        ringsdbwriter.writerow(ringsdbrow)
+        octgnwriter.writerow(octgnrow)
         
 # Close file
-beornCSV.close()
+ringsdbCSV.close()
+octgnCSV.close()
