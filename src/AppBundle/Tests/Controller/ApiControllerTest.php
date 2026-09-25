4d58620a263dc2bf8352cb0211fb7000793cc718
Alsciende <alsciende@icloud.com>
2026-09-25 10:35:08 +0200

    run tests in the absence of proper fixtures

--- src/AppBundle/Tests/Controller/ApiControllerTest.php
+++ src/AppBundle/Tests/Controller/ApiControllerTest.php
@@ -62,7 +62,15 @@ class ApiControllerTest extends WebTestCase {
 
     public function testGetDecklist() {
         $client = static::createClient();
-        $client->request('GET', '/api/public/decklist/1');
+
+        $id = static::$kernel
+            ->getContainer()
+            ->get('doctrine')
+            ->getConnection()
+            ->executeQuery("SELECT id FROM decklist LIMIT 1")
+            ->fetchColumn();
+
+        $client->request('GET', '/api/public/decklist/' . $id);
         $response = $client->getResponse();
         $json = $response->getContent();
         $this->assertJson($json);
@@ -70,12 +78,20 @@ class ApiControllerTest extends WebTestCase {
         $this->assertNotNull($data);
         $this->assertInternalType('array', $data);
         $this->assertArrayHasKey("id", $data);
-        $this->assertEquals(1, $data['id']);
+        $this->assertEquals($id, $data['id']);
     }
 
     public function testListDecklists() {
         $client = static::createClient();
-        $client->request('GET', '/api/public/decklists/2015-08-16');
+
+        $date = static::$kernel
+            ->getContainer()
+            ->get('doctrine')
+            ->getConnection()
+            ->executeQuery("SELECT DATE_FORMAT(date_creation, '%Y-%m-%d') FROM decklist LIMIT 1")
+            ->fetchColumn();
+
+        $client->request('GET', '/api/public/decklists/by_date/' . $date);
         $response = $client->getResponse();
         $json = $response->getContent();
         $this->assertJson($json);
@@ -86,7 +102,7 @@ class ApiControllerTest extends WebTestCase {
         foreach ($data as $item) {
             $this->assertInternalType('array', $item);
             $this->assertArrayHasKey("date_creation", $item);
-            $this->assertStringStartsWith('2015-08-16', $item['date_creation']);
+            $this->assertStringStartsWith($date, $item['date_creation']);
         }
     }
 }
